2147a78ea6da18343a3baf2233674d23c83ccf23
Ali <203810191+K3maal@users.noreply.github.com>
2026-09-24 12:01:12 +0200

    adding a request limiter

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,20 @@ from database import init_db
 from routes.auth import auth_bp
 from routes.student import student_bp
 from routes.docent import docent_bp
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+
 
 app = Flask(__name__)
 CORS(app)
+
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["2000 per day" , "100 per minute"],
+    storage_uri= "memory://",
+)
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(student_bp)
